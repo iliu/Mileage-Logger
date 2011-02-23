e21601070684b809704927064dcad35737925f4d
Isaac Liu <liu.isaac@gmail.com>
2011-02-23 14:28:15 -0800

    renamed files and added style sheet added autocomplete

---
+++ js/logger.js
@@ -0,0 +1,33 @@
+/**
+ * @author liuisaac
+ */
+
+ $(function() {
+		var schools = [
+			"ActionScript",
+			"AppleScript",
+			"Asp",
+			"BASIC",
+			"C",
+			"C++",
+			"Clojure",
+			"COBOL",
+			"ColdFusion",
+			"Erlang",
+			"Fortran",
+			"Groovy",
+			"Haskell",
+			"Java",
+			"JavaScript",
+			"Lisp",
+			"Perl",
+			"PHP",
+			"Python",
+			"Ruby",
+			"Scala",
+			"Scheme"
+		];
+		$( ".schools" ).autocomplete({
+			source: schools
+		});
+	});
